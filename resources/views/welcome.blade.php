f9939fcbb3fb18c29e8c0a8f8aa0507639d35f0a
Kalbir Sohi <kalbir@users.noreply.github.com>
2017-10-17 23:16:48 +0100

    Make the title "kalbir test"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kalbir Test
                 </div>
 
                 <div class="links">
